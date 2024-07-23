be5f6c9a70758238153f2f3b7c9ca442a1e5fbbc
o2buzzle <76864037+o2buzzle@users.noreply.github.com>
2024-07-23 11:23:07 +0700

    added: solver

--- src/solver/roialign/roialign.cpp
+++ src/solver/roialign/roialign.cpp
@@ -26,9 +26,14 @@
 
 #include "miopen/conv_solution.hpp"
 #include "miopen/execution_context.hpp"
+#include "miopen/kernel_build_params.hpp"
 #include "miopen/miopen.h"
+#include "miopen/roialign/invoke_params.hpp"
 #include "miopen/roialign/problem_description.hpp"
 #include "miopen/roialign/solvers.hpp"
+#include "miopen/tensor_view_utils.hpp"
+
+#define ROIALIGN_LOCAL_SIZE 256
 
 namespace miopen {
 namespace solver {
@@ -55,6 +60,65 @@ ConvSolution RoIAlignForward::GetSolution(const ExecutionContext& context,
     auto input_dims  = problem.GetInputDesc().GetLengths();
     auto rois_dims   = problem.GetRoisDesc().GetLengths();
     auto output_dims = problem.GetOutputDesc().GetLengths();
+
+    const size_t K = problem.GetRoisDesc().GetLengths()[0];
+    const size_t C = problem.GetInputDesc().GetLengths()[1];
+    const size_t g =
+        K * C * problem.GetAlignedHeight() * problem.GetAlignedWidth() / ROIALIGN_LOCAL_SIZE;
+
+    const size_t xlocalsize = g + 1;
+    const size_t ylocalsize = 1;
+    const size_t zlocalsize = 1;
+
+    const size_t xgridsize = ROIALIGN_LOCAL_SIZE;
+    const size_t ygridsize = 1;
+    const size_t zgridsize = 1;
+
+    auto kernel        = KernelInfo{};
+    kernel.kernel_file = "MIOpenRoIAlign.cpp";
+    kernel.kernel_name = "RoIAlignForward";
+
+    const auto build_params = KernelBuildParameters{
+        {"MIOPEN_USE_FP16", static_cast<int32_t>(dtype == miopenHalf)},
+        {"MIOPEN_USE_FP32", static_cast<int32_t>(dtype == miopenFloat)},
+        {"MIOPEN_USE_FP64", static_cast<int32_t>(dtype == miopenDouble)},
+        {"MIOPEN_USE_BFP16", static_cast<int32_t>(dtype == miopenBFloat16)},
+    };
+
+    kernel.comp_options = build_params.GenerateFor(kbp::HIP{});
+
+    kernel.l_wk.push_back(xlocalsize);
+    kernel.l_wk.push_back(ylocalsize);
+    kernel.l_wk.push_back(zlocalsize);
+
+    kernel.g_wk.push_back(xgridsize);
+    kernel.g_wk.push_back(ygridsize);
+    kernel.g_wk.push_back(zgridsize);
+
+    result.construction_params.push_back(kernel);
+
+    result.invoker_factory = [](const std::vector<Kernel>& kernels) {
+        return [=](const Handle& handle_, const AnyInvokeParams& raw_params) {
+            decltype(auto) params = raw_params.CastTo<miopen::roialign::InvokeParams>();
+            decltype(auto) kernel = handle_.Run(kernels[0]);
+
+            auto input_tv  = get_inner_expanded_tv<4>(*params.inputDesc);
+            auto rois_tv   = get_inner_expanded_tv<2>(*params.roisDesc);
+            auto output_tv = get_inner_expanded_tv<4>(*params.outputDesc);
+
+            kernel(params.input,
+                   params.rois,
+                   params.output,
+                   params.alignedHeight,
+                   params.alignedWidth,
+                   params.spatialScale,
+                   params.samplingRatio,
+                   params.roi_batch_index,
+                   input_tv,
+                   rois_tv,
+                   output_tv);
+        };
+    };
 
     return result;
 }
